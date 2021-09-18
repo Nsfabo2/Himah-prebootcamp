026f0c1852a8e4f79e9905679abbbe43a6f88789
Najla Abothnain <60915559+Nsfabo2@users.noreply.github.com>
2021-09-18 19:02:31 +0300

    Add files via upload

---
+++ Abothnain_Najla_JS_CreateVariables.js
@@ -0,0 +1,12 @@
+/*Step 1: Identify what variables are needed to determine if a rider can ride the rollercoaster.
+#1 an age variable 
+#2 a height variable */
+// Step 2:  create those variables.
+var height;
+var age;
+/* Step 3: explain what the variables must be set to in order for the rider to ride. 
+For the rider to get on the rollercoaster he/she needs to be
+42 inches or taller and older than 10 years old 
+height => 42
+age > 10   */
+
